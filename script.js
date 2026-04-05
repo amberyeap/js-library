b2771c92d8c60283ce0c70638126b48d5ebef884
amberyeap <amberyeap02@gmail.com>
2026-04-06 02:30:39 +0800

    feat: added a function (addBookToLibrary)
    This function automatically creates an object and adds the object to the existing array, myLibrary

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+let myLibrary = [];
 
 function Book(id, title, author, numPages, ifRead) {
 	this.id = id;
@@ -15,9 +15,20 @@ function Book(id, title, author, numPages, ifRead) {
 	}
 }
 
-function addBookToLibrary() {
-	
+function addBookToLibrary(title, author, numPages, ifRead) {
+	const newBook = new Book(crypto.randomUUID(), title, author, numPages, ifRead);
+
+	myLibrary.push(newBook);
 }
+
+// addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+// console.log(myLibrary);
+// console.log(myLibrary[0].info());
+
+// addBookToLibrary("Harry Potter", "J.K. Rowling", 500, true);
+// console.log(myLibrary);
+// console.log(myLibrary[0].info());
+// console.log(myLibrary[1].info());
 
 // const theHobbit = new Book(crypto.randomUUID(), "The Hobbit", "J.R.R. Tolkien", 295, false);
 
